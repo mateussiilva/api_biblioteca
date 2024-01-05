e84475753350e48370b436fea865cf7c57ce3ea7
mateussiilva <mateuspython3010@gmail.com>
2024-01-04 22:01:32 -0300

    Criando novas rotas e melhorando a logica

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask,request
 from main import PyJson
 
 
@@ -6,11 +6,27 @@ app = Flask(__name__)
 pyjson = PyJson("utf-8")
 livros = pyjson.readjson("models/base.json")
 
-@app.route("/")
+@app.route("/",methods=["GET"])
 def home():
-    return render_template("home.html",livros=livros)
+    return livros
 
+@app.route("/livros",methods=["GET"])
+def quantidade_livros():
+    return f"{len(livros)} Livros cadastrados"
 
+@app.route("/<int:id>", methods=["GET","POST"])
+def buscar_livro(id: int):
+    ids_livros = len(livros)
+    if id > 0 and id <= ids_livros:
+        return livros.get(str(id))
+    else:
+        return f"[ERRO] o ID '{id}' não existe no banco de dados".upper()
+    
+@app.route("/cadastrar/")
+def cadastrar_livro():
+    return request.args.to_dict
+    
+    
 
 if __name__ == '__main__':
    app.run(debug=True)
