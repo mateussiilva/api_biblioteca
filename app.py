26cfe65842b6b80fcc18cf8ef2b73f42c31e14d7
mateussiilva <mateuspython3010@gmail.com>
2024-02-05 20:45:40 -0300

    Conectando backend com frontend

--- app.py
+++ app.py
@@ -1,24 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request,redirect,url_for
 from main import PyJson 
 
 
 app = Flask(__name__)
 pyjson = PyJson("utf-8")
-livros = pyjson.readjson("../models/base.json")
+livros = pyjson.readjson("models/base.json")
 
 @app.route("/",methods=["GET"])
 def home():
-    # return jsonify(livros)
     return render_template("index.html")
 
 
-@app.route("/livros",methods=["GET"])
-def quantidade_livros():
-    titulos = tuple(map(lambda id: livros[id]["titulo"],livros))
-    return f"{titulos}"
+@app.route("/cadastrar",methods=["POST","GET"])
+def cadastrar():
+    if request.method == "GET":
+        return render_template("cadastro.html")
+    if request.method == "POST":
+        nomde_de_livro = request.form['nomdeDoLivro']
+        qtd_paginas = request.form['quntiadeDePginas']
+        
+        return redirect(url_for("redi"))
+        
+        # return f"{nomde_de_livro},{qtd_paginas}"
 
 
-
+@app.route("/redi")
+def redi():
+    return "Estou na pagina de redirect"
     
 
 if __name__ == '__main__':
